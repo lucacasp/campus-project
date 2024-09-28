f3eb26bc90c286189ed1257283db8f8af7391f29
Luca Casparriello <155079990+lucacasp@users.noreply.github.com>
2024-09-28 21:26:31 +0200

    implementazione videoplayer, + pacchetti react router

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,14 @@ import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false);
+
+
   return (
     <div>
       <Navbar />
@@ -17,7 +23,7 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our Program' title='What we offer' />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus />
         <Title subTitle='Testimonials' title='What student says' />
@@ -26,6 +32,7 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   )
 }
---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,19 @@
+import React, { useRef } from 'react'
+import './VideoPlayer.css'
+import video from '../../Assets/video.mp4'
+
+const VideoPlayer = ({ playState, setPlayState }) => {
+    const player = useRef(null);
+
+    const closePlayer = () => {
+      setPlayState(false)
+    }
+  return (
+    <div className={`video-player ${playState?'':'hide'}`}>
+      <video src={video} autoPlay muted controls></video>
+      <div className="close-btn" onClick={closePlayer}>Chiudi</div>
+    </div>
+  )
+}
+
+export default VideoPlayer
